f7dd1d6e4c36475da98fab1df2e10b4e35be4bf7
giovanni.caini <gcaini95@gmail.com>
2019-02-07 02:14:03 +0100

    Creato asl.c
    Creato asl.c da asl.h
    Inseriti header e commenti dalle slides

---
+++ asl.c
@@ -0,0 +1,46 @@
+/************************************/
+/* Progetto  SO 2019 - Rikaya       */
+/* Gruppo lso19az18                 */
+/* Ultimo agg: Gio 07/02 			*/
+/************************************/
+
+/**************************/
+/* ASL handling functions */
+/**************************/
+
+/* 14 -	Inserisce il PCB puntato da p nella lista dei PCB liberi (pcbFree) */
+semd_t* getSemd(int *key){
+
+}
+
+/* 15 -	Inserisce il PCB puntato da p nella lista dei PCB liberi (pcbFree) */
+void initASL(){
+
+}
+
+/* 16 -	Inserisce il PCB puntato da p nella lista dei PCB liberi (pcbFree) */
+int insertBlocked(int *key,pcb_t* p){
+
+}
+
+/* 17 -	Inserisce il PCB puntato da p nella lista dei PCB liberi (pcbFree) */
+pcb_t* removeBlocked(int *key){
+
+}
+
+/* 18 -	Inserisce il PCB puntato da p nella lista dei PCB liberi (pcbFree) */
+pcb_t* outBlocked(pcb_t *p){
+
+}
+
+/* 19 -	Inserisce il PCB puntato da p nella lista dei PCB liberi (pcbFree) */
+pcb_t* headBlocked(int *key){
+
+}
+
+/* 20 -	Inserisce il PCB puntato da p nella lista dei PCB liberi (pcbFree) */
+void outChildBlocked(pcb_t *p){
+
+}
+
+#endif
